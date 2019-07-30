2f897d83f2f62a4628ade255fddac4a2980b0752
Daniel Hornberger <daniel.hornberger@sdl.usu.edu>
2019-07-30 13:14:32 -0600

    Fixed syntax error

--- setupDevEnv.sh
+++ setupDevEnv.sh
@@ -12,7 +12,7 @@ echo "        ---   -----                      "
 echo
 echo
 
-echo Do you want to restore the previous setup? (Entering 'n' will run the setup) [y/n]
+echo "Do you want to restore the previous setup? (Entering 'n' will run the setup)" [y/n]
 read doRestore
 echo
 
