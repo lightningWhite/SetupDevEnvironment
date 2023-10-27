8441c9b0ccdb50d11737436f1c8e2c03f280a46e
lightningWhite <lightningWhite@duck.com>
2023-10-27 09:17:23 -0600

    Install cmake apt repo and update script a bit

--- setupVim.sh
+++ setupVim.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
 # Make sure vim is installed and vim-gtk3 (for clipboard support)
-echo "Installing vim and vim-gtk3..."
+echo "*** Installing vim and vim-gtk3... ***"
 sudo apt-get install vim vim-gtk3
 
 # Install vim-plug Package Manager
-echo "Installing the vim-plug plugin manager..."
+echo "*** Installing the vim-plug plugin manager... ***"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Copy the custom colorscheme to the right location
+echo "*** Seting up custom colorscheme... ***"
 mkdir ~/.vim/colors
 cp ./lightningWhite.vim ~/.vim/colors
 
-echo "Backing up the existing .vimrc and copying the .vimrc in this repo to ~/.vimrc..."
+echo "*** Backing up the existing .vimrc and copying the .vimrc in this repo to ~/.vimrc... ***"
 mv ~/.vimrc ~/.vimrc_$(date +"%Y%m%d_%H%M%S")
 cp ./.vimrc ~/.vimrc
 
 # Install the plugins in the .vimrc file by running the following in vim
 # (equivalent to opening vim and running :PlugInstall)
-echo "Installing the plugins listed in the .vimrc file..."
+echo "*** Installing the plugins listed in the .vimrc file... ***"
 vim +'PlugInstall --sync' +qa
 
-# Install dependencies and compile YouCompleteMe
-echo "Compiling and setting up YouCompleteMe..."
-sudo apt install build-essential cmake vim-nox python3-dev npm golang clang-format-11
+## Setup YouCompleteMe ##
+
+echo "*** Installing cmake... ***"
+# See https://apt.kitware.com/ for installing cmake (required for building YouCompleteMe)
+sudo apt-get update && sudo apt-get install ca-certificates gpg wget
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/nulla
+# Specific to Ubuntu Jammy - modify if needed
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ jammy main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+sudo apt-get update
+sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
+sudo apt-get install kitware-archive-keyring
+sudo apg-get install cmake
+
+# Install other dependencies
+echo "*** Installing other YouCompleteMe dependencies... ***"
+sudo apt-get install build-essential vim-nox python3-dev npm golang clang-format-11 openjdk-19-jre
+
+echo "*** Compiling and setting up YouCompleteMe... ***"
 (cd ~/.vim/plugged/YouCompleteMe && ./install.py --all)
+
+echo "*** Done setting up development environment! ***"
