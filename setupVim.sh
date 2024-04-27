534262e27824ffb300bb89605b7e7f1479c77a07
Daniel Hornberger <22121347+lightningWhite@users.noreply.github.com>
2024-04-27 08:22:33 -0600

    Fix bug in kitware key stuff

--- setupVim.sh
+++ setupVim.sh
@@ -28,7 +28,7 @@ vim +'PlugInstall --sync' +qa
 echo "*** Installing cmake... ***"
 # See https://apt.kitware.com/ for installing cmake (required for building YouCompleteMe)
 sudo apt-get update && sudo apt-get install ca-certificates gpg wget
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/nulla
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 # Specific to Ubuntu Jammy - modify if needed
 echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ jammy main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update
